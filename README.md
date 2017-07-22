c16718e427473a85152d4ce37c6124f9711fcf4a
Krzysztof Jankowski <w84death@gmail.com>
2017-07-22 13:22:10 +0200

    mietek 0.3

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # piot
 Raspberry Pi IoT Software - Slack Chatbot
+
+## Mietek
+
+First real chatbot that is created mainly for testing purposes.
+
+
--- mietek.py
+++ mietek.py
@@ -1,72 +1,93 @@
-#
-# MIETEK ZUL BOT
-# (c)2017 KJ/P1X
-#
+#!/bin/python
+# -----------------
+#     ~MIETEK~ 
+#    the chatbot
+# -----------------
+#  (c)2017 KJ/P1X
 
-import os
-import time
-from random import randint
-from slackclient import SlackClient
+import os, slackclient, time, re, random
 
-# SLACK CLIENT STUFF
-TOKEN = os.environ.get('SLACK_MIETEK_TOKEN')
-BOT_ID = os.environ.get('BOT_MIETEK_ID')
-sc = SlackClient(TOKEN)
+SOCKET_DELAY = 1
+BOT_SLACK_NAME = 'mietek'
+BOT_SLACK_TOKEN = os.environ.get('SLACK_MIETEK_TOKEN')
+BOT_SLACK_ID = os.environ.get('BOT_MIETEK_ID')
+sc = slackclient.SlackClient(BOT_SLACK_TOKEN)
+def get_mention(user):
+    return '<@{user}>'.format(user=user)
+bot_slack_mention = get_mention(BOT_SLACK_ID)
 
-# SETTINGS
-READ_WEBSOCKET_DELAY = 1
-
-# COMMANDS
 CMDS = [
     [
-        ['witam', 'czesc', 'hi', 'yo', 'elo', 'siema'],['Witam pana kierownika!', 'Uszanowanie!']
+        ['witam', 'czesc', 'hi', 'yo', 'elo', 'siema'],
+        ['Witam pana kierownika!', 'Uszanowanie!']
     ],
     [
-        ['wino', 'winiacz', 'piwo', 'piwko', 'browar'],['Alkohol?', 'Napil bym sie...']
+        ['wino', 'winiacz', 'piwo', 'piwko', 'browar'],
+        ['Alkohol?', 'Napil bym sie...']
     ],
     [
-        ['chuju'],['A po co zaraz tak wulgarnie?', 'Taki mocny w gebie to moze poratujesz 50gr?']
+        ['chuj', 'chuju', 'burak', 'debil'],
+        ['A po co zaraz tak wulgarnie?', 'Taki mocny w gebie to moze poratujesz 50gr?']
     ]
 ]
 
-# THE BRAIN OF MIETEK
-
-def handle_command(command, channel):
-    command = command.lower()
-    
-    print (command + " at " + channel)
+def decode_message(message):
+    tokens = [re.sub('[^A-Za-z0-9]+', '', word.lower()) for word in message.strip().split()]
     for cmd in CMDS:
-        for cmd_code in cmd[0]:
-             if command.startswith(cmd_code):
-                say(cmd[1], channel)
+        if any(g in tokens for g in cmd[0]):
+            return random.choice(cmd[1])
+    return False
 
-def say(reply, channel):
-    reply_message = reply[randint(0,len(reply)-1)]
-    sc.api_call(
-        "chat.postMessage", 
-        as_user="true", 
-        channel=channel, 
-        text=reply_message)
+def format_response(response, user_mention):
+    return response.format(mention=user_mention)
 
-def parse_slack_output(slack_rtm_output):
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and not 'bot_id' in output:
-                return output['text'], output['channel']
-    return None, None
+def handle_message(message, user, channel):
+    message_decoded = decode_message(message)
+    if message_decoded:
+        user_mention = get_mention(user)
+        post_message(message=format_response(message_decoded, user_mention), channel=channel)
 
+def is_for_me(event):
+    type = event.get('type')
+    if type and type == 'message' and not(event.get('user') == BOT_SLACK_ID):
+        if is_private(event):
+            return True
+        text = event.get('text')
+        channel = event.get('channel')
+        if bot_slack_mention in text.strip().split():
+            return True
+    return False
 
-# MAIN
+def is_private(event):
+    return event.get('channel').startswith('D')
 
-if __name__ == "__main__":
+def post_message(message, channel):
+    sc.api_call('chat.postMessage', channel=channel,
+                          text=message, as_user=True)
+
+def format_log(event):
+    log = '[+] ' + str(event.get('type'))
+    if event.get('user'):
+        log += ' <' + str(event.get('user')) + '> '
+    if event.get('text'):
+        log += str(event.get('text'))
+    return log
+def run():
     if sc.rtm_connect():
-        print ("Mietek zyje!")
-        
+        print('[.] Mietek is alive!')
         while True:
-            command, channel = parse_slack_output(sc.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
+            event_list = sc.rtm_read()
+            if len(event_list) > 0:
+                for event in event_list:
+                    print(format_log(event))
+                    if is_for_me(event):
+                        handle_message(message=event.get('text'), user=event.get('user'), channel=event.get('channel'))
+            time.sleep(SOCKET_DELAY)
     else:
-        print("Connection failed.")
+        print('[!] Connection to Slack failed.')
+
+
+if __name__=='__main__':
+    print('[.] ~MIETEK~ the chatbot v.03')
+    print('[.] Booting Mietek..')
+    run()
